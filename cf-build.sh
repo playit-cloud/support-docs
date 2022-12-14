d0930f8ad085af0f93788a0360351e322c7c4f3d
Patrick Lorio <dev@plorio.com>
2022-12-14 08:50:00 -0800

    Add script to redirect page

--- cf-build.sh
+++ cf-build.sh
@@ -8,6 +8,7 @@ echo "
 <html>
   <head>
     <meta http-equiv=\"refresh\" content=\"7; url='/support'\" />
+    <script>window.location = '/support';</script>
   </head>
   <body>
     <p>Please follow <a href=\"/support\">this link</a>.</p>
