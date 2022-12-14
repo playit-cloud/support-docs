61afb3022ae9a43a94d2d6391187bb75d2127d3a
Patrick Lorio <dev@plorio.com>
2022-12-14 08:48:05 -0800

    Make navbar relative and redirect cloudflare /

--- cf-build.sh
+++ cf-build.sh
@@ -3,5 +3,17 @@ hugo
 rm -rf out
 mkdir out
 
+echo "
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv=\"refresh\" content=\"7; url='/support'\" />
+  </head>
+  <body>
+    <p>Please follow <a href=\"/support\">this link</a>.</p>
+  </body>
+</html>
+" > out/index.html
+
 mv public out/support
 
